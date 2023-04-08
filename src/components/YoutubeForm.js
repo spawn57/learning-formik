58f0a4ef94d5b23152ffe6b24fa4437e6eebfd7d
Sunil Khiatani <spawn57@yahoo.com>
2023-04-08 16:23:36 +0800

    added onsubmit handler

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -8,14 +8,15 @@ function YoutubeForm() {
             name: 'Sunil',
             email: '',
             channel: '',
+        },
+        onSubmit: values => {
+            console.log('form data:', values);
         }
     });
 
-    console.log('Form values', formik.values);
-
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor='name'>Name</label>
                 <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
 
@@ -25,7 +26,7 @@ function YoutubeForm() {
                 <label htmlFor='channel'>Channel</label>
                 <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
 
-                <button>Submit</button>
+                <button type='submit'>Submit</button>
             </form>
         </div>
     );
