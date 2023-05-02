4aef9e4f04703071d7cf2e7cafd2ce379f8f25d2
Sunil Khiatani <skhiatani@gmail.com>
2023-05-02 20:24:41 +0800

    merging my changes between two computers

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -10,30 +10,47 @@ function FormikContainer() {
         { key: 'option3', value: 'Option 3' },
     ]
     const initialValues = {
-        email: '',
-        description: '',
-    }
+      email: "",
+      description: "",
+      radioOption: "",
+    };
     const validationSchema = Yup.object({
         email: Yup.string().required('Required'),
-        description: Yup.string().required('Required')
+        description: Yup.string().required('Required'),
+        radioOption: Yup.string().required('Required'),
     });
     const onSubmit = values => console.log('Form data', values);
     
     return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-        >
-            {
-                formik => <Form>
-                    <FormikControl control='input' type='email' label='Email' name='email' />
-                    <FormikControl control='textarea' label='Description' name='description' /> 
-                    <button type='submit'>Submit</button>
-                </Form>
-            }
-        </Formik>
-    )
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(formik) => (
+          <Form>
+            <FormikControl
+              control="input"
+              type="email"
+              label="Email"
+              name="email"
+            />
+            <FormikControl
+              control="textarea"
+              label="Description"
+              name="description"
+            />
+            <FormikControl
+              control="radio"
+              label="Radio Topic"
+              name="radioOption"
+              options={radioOptions}
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    );
 }
 
 export default FormikContainer;
