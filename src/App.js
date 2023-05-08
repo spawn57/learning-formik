43b053c250030dd4a4dbef970dadf6a094833f17
Sunil Khiatani <skhiatani@gmail.com>
2023-05-08 20:37:53 +0800

    added registration form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <RegistrationForm />
     </div>
   );
 }
